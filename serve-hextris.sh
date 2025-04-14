e26afcba3a503f118f3c533ca587d943a53fa56c
RAHUL SAGAR PABBA <rahulsagarpabba5@gmail.com>
2025-04-14 22:59:34 +1000

    added more dependecies for passing tests

--- serve-hextris.sh
+++ serve-hextris.sh
@@ -4,5 +4,7 @@ systemctl enable httpd
 systemctl start httpd 
  
 yum install -y git 
+yum install -y python3-pip
+pip3 install pipenv
 cd /var/www/html 
 git clone https://github.com/Hextris/hextris .
