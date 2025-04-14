064ceceb73f85e4f003ed174a0b3e04a8734f358
RAHUL SAGAR PABBA <rahulsagarpabba5@gmail.com>
2025-04-14 23:24:38 +1000

    trying to resolve tests

--- serve-hextris.sh
+++ serve-hextris.sh
@@ -1,10 +1,8 @@
-#!/bin/bash 
-yum install -y httpd 
-systemctl enable httpd 
-systemctl start httpd 
- 
-yum install -y git 
-yum install -y python3-pip
-pip3 install pipenv
-cd /var/www/html 
+#!/bin/bash
+yum install -y httpd
+systemctl enable httpd
+systemctl start httpd
+
+yum install -y git
+cd /var/www/html
 git clone https://github.com/Hextris/hextris .
